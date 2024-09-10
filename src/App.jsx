54138a21c0e94942a675dea87f30ef21d794a204
kkimhyorr <hyorie03@naver.com>
2024-09-11 03:43:12 +0900

    Feat: Footer 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import { Theme } from "./styles/Theme";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import TabLayout from "./pages/TabLayout";
 import TabContent from "./components/TabContent";
@@ -21,6 +22,7 @@ const App = () => {
             </Route>
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </ThemeProvider>
   );
